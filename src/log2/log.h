e42937f7d994bc1359b46b70cc8f8e76e534cbab
catzzz <2680417551@qq.com>
2024-12-09 17:44:19 +0800

    2024/12/09 17:40  --日常提交 无重要描述

--- src/log2/log.h
+++ src/log2/log.h
@@ -13,6 +13,7 @@
 #include <memory>
 #include <string>
 #include <sstream>
+#include <vector>
 
 #define CATNET_LOG_LEVEL(logger, level) \
     if(level>=logger->getLevel()) catnet::LogWrap(logger).getLogger()->getStream()
@@ -24,150 +25,187 @@
 #define CATNET_LOG_FATAL(logger) CATNET_LOG_LEVEL(logger,catnet::LogLevel::FATAL)
 
 namespace catnet {
+class Logger;
+
 class LogLevel {
 public:
-    enum Level {
-        DEBUG, INFO, WARN, ERROR, FATAL, UNKNOWN
-    };
-    
-    /**
-    * 返回值为char*而不是std::string，因为printf()的形参%s为char*类型
-    */
-    static const char *toString(Level level);
+	enum Level {
+		DEBUG, INFO, WARN, ERROR, FATAL, UNKNOWN
+	};
+
+	/**
+	* 返回值为char*而不是std::string，因为printf()的形参%s为char*类型
+	*/
+	static const char *toString(Level level);
 };
 
 class LogEvent {
 public:
-    LogEvent(const char *mFile, int32_t mLine, int32_t mElapse, int32_t mThreadId, int32_t mFiberId,
-             uint64_t mTimestamp, LogLevel::Level mLevel, std::stringstream& mContent)
-            : m_file(mFile), m_line(mLine), m_elapse(mElapse),
-              m_thread_id(mThreadId), m_fiber_id(mFiberId),
-              m_timestamp(mTimestamp), m_level(mLevel), m_content(mContent) {}
-    
-    const char *getFile() const {
-        return m_file;
-    }
-    
-    int32_t getLine() const {
-        return m_line;
-    }
-    
-    int32_t getElapse() const {
-        return m_elapse;
-    }
-    
-    int32_t getThreadId() const {
-        return m_thread_id;
-    }
-    
-    int32_t getFiberId() const {
-        return m_fiber_id;
-    }
-    
-    uint64_t getTimestamp() const {
-        return m_timestamp;
-    }
-    
-    LogLevel::Level getLevel() const {
-        return m_level;
-    }
-    
-    std::stringstream& getContent() const {
-        return m_content;
-    }
+	LogEvent(const char *mFile, int32_t mLine, int32_t mElapse, int32_t mThreadId, int32_t mFiberId,
+			 uint64_t mTimestamp, LogLevel::Level mLevel, std::stringstream& mContent)
+		: m_file(mFile), m_line(mLine), m_elapse(mElapse),
+		  m_thread_id(mThreadId), m_fiber_id(mFiberId),
+		  m_timestamp(mTimestamp), m_level(mLevel), m_content(mContent) {
+	}
+
+	const char *getFile() const {
+		return m_file;
+	}
+
+	std::shared_ptr<Logger> logger() const {
+		return m_logger;
+	}
+
+	int32_t getLine() const {
+		return m_line;
+	}
+
+	int32_t getElapse() const {
+		return m_elapse;
+	}
+
+	int32_t getThreadId() const {
+		return m_thread_id;
+	}
+
+	int32_t getFiberId() const {
+		return m_fiber_id;
+	}
+
+	uint64_t getTimestamp() const {
+		return m_timestamp;
+	}
+
+	LogLevel::Level getLevel() const {
+		return m_level;
+	}
+
+	std::stringstream& getContent() const {
+		return m_content;
+	}
 
 private:
-    const char *m_file = nullptr;
-    int32_t m_line = 0;
-    int32_t m_elapse = 0;     // 程序从启动开始到现在的毫秒数
-    int32_t m_thread_id = 0;
-    int32_t m_fiber_id = 0;
-    uint64_t m_timestamp = 0;
-    LogLevel::Level m_level;
-    std::stringstream& m_content;
+	const char *m_file = nullptr;
+	std::shared_ptr<Logger> m_logger;	// 该事件所属日志器
+	int32_t m_line = 0;
+	int32_t m_elapse = 0;     // 程序从启动开始到现在的毫秒数
+	int32_t m_thread_id = 0;
+	int32_t m_fiber_id = 0;		// 协程ID
+	uint64_t m_timestamp = 0;	// 时间戳
+	LogLevel::Level m_level;
+	std::stringstream& m_content;
 };
 
 class FormatItem {
 public:
-    typedef std::shared_ptr<FormatItem> ptr;
-    
-    virtual ~FormatItem() = default;
+	typedef std::shared_ptr<FormatItem> ptr;
+
+	explicit FormatItem(const std::string &format)
+		: m_format(format) {
+	}
+
+	// 当基类拥有虚函数，并且希望确保派生类可以被正确删除时，基类的虚构函数应该是虚拟的
+	// = default让编译器生成一个适当的默认析构函数
+	virtual ~FormatItem() = default;
 
 protected:
-    std::string m_format;
+	const std::string m_format;
 };
 
-class MessageFormatItem : public FormatItem {
-void func(){
-    m_format;
-}
+class MessageFormatItem final : public FormatItem {
+public:
+	MessageFormatItem(): FormatItem("") {
+	}
+
+private:
+};
+
+class LoggerNameFormatItem final : public FormatItem {
+public:
+	LoggerNameFormatItem():FormatItem("") {
+	}
 };
 
 class LogFormatter {
 public:
-    typedef std::shared_ptr<LogFormatter> ptr;
-    
-    explicit LogFormatter(const std::string& format_str) : m_format_str(format_str) {}
-    
-    void pattern(const std::string& format_str = "");
+	typedef std::shared_ptr<LogFormatter> ptr;
+
+	explicit LogFormatter(const std::string& format_str) : m_format_str(format_str) {
+	}
+
+	[[nodiscard]] std::string getFormatStr() const {
+		return m_format_str;
+	}
+
+	[[nodiscard]] std::vector<FormatItem> getItems() const {
+		return m_items;
+	}
+
+	/**
+	 * 解析格式化字符串的每一项，存储到m_items中
+	 * @param format_str 待解析的格式化字符串
+	 */
+	void pattern(const std::string& format_str = "");
+
 
 private:
-    std::string m_format_str;  // 格式化字符串
+	/*
+	 * 在初始化时需要确定好输出格式，即在构造函数中确定m_format_str和m_items的内容
+	 */
+	std::string m_format_str;  // 格式化字符串
+	std::vector<FormatItem> m_items;	// 按顺序存储格式化字符串的每一项，输出时按照m_items元素顺序逐个输出
 };
 
 class LogAppender {
-
 };
 
 class StdoutLogAppender : LogAppender {
-
 };
 
 class Logger {
 public:
-    typedef std::shared_ptr<Logger> ptr;
-    
-    explicit Logger(std::string name) : m_name(std::move(name)), m_level(LogLevel::Level::INFO) {
-    }
-    
-    ~Logger() = default;
-    
-    [[nodiscard]] const std::string& getName() const { return m_name; }
-    
-    [[nodiscard]] LogLevel::Level getLevel() const { return m_level; }
-    
-    void setLevel(const LogLevel::Level level) { m_level = level; }
-    
-    [[nodiscard]] std::stringstream& getStream() {
-        return m_stream;
-    }
-    
-    void log();
+	typedef std::shared_ptr<Logger> ptr;
+
+	explicit Logger(std::string name) : m_name(std::move(name)), m_level(LogLevel::Level::INFO) {
+	}
+
+	~Logger() = default;
+
+	[[nodiscard]] const std::string& getName() const { return m_name; }
+
+	[[nodiscard]] LogLevel::Level getLevel() const { return m_level; }
+
+	void setLevel(const LogLevel::Level level) { m_level = level; }
+
+	[[nodiscard]] std::stringstream& getStream() {
+		return m_stream;
+	}
+
+	void log();
 
 private:
-    std::string m_name;
-    LogLevel::Level m_level;
-    std::stringstream m_stream;
+	std::string m_name;
+	LogLevel::Level m_level;
+	std::stringstream m_stream;
 };
 
 class LogWrap {
 public:
-    explicit LogWrap(Logger::ptr& logger)
-            : m_logger(logger) {
-    }
-    
-    ~LogWrap() {
-        m_logger->log();
-    }
-    
-    [[nodiscard]] Logger::ptr getLogger() const {
-        return m_logger;
-    }
+	explicit LogWrap(Logger::ptr& logger)
+		: m_logger(logger) {
+	}
+
+	~LogWrap() {
+		m_logger->log();
+	}
+
+	[[nodiscard]] Logger::ptr getLogger() const {
+		return m_logger;
+	}
 
 private:
-    Logger::ptr m_logger;
+	Logger::ptr m_logger;
 };
-
 }// namespace catnet
 
 #endif// LOG_H
--- test/test_log.cpp
+++ test/test_log.cpp
@@ -19,48 +19,54 @@
 // }
 
 std::vector<std::string> split_string(const std::string& input_str) {
-    std::vector<std::string> result;
-    
-    // 定义正则表达式：匹配转义项 % 后跟一个字母或 % 或普通字符项
-    std::regex pattern(R"((%[a-zA-Z%])|(%[^a-zA-Z%])|([^%]+))");
-    
-    // 使用 std::sregex_iterator 查找所有匹配项
-    auto begin = std::sregex_iterator(input_str.begin(), input_str.end(), pattern);
-    auto end = std::sregex_iterator();
-    
-    // 遍历匹配结果，并将其存储在 result 中
-    for (auto it = begin; it != end; ++it) {
-        result.push_back(it->str());
-    }
-    std::cout<<input_str;
-    return result;
+	std::vector<std::string> result;
+
+	// 定义正则表达式：匹配转义项 % 后跟一个字母或 % 或普通字符项
+	std::regex pattern(R"((%[a-zA-Z%])|(%[^a-zA-Z%])|([^%]+))");
+
+	// 使用 std::sregex_iterator 查找所有匹配项
+	auto begin = std::sregex_iterator(input_str.begin(), input_str.end(), pattern);
+	auto end = std::sregex_iterator();
+
+	// 遍历匹配结果，并将其存储在 result 中
+	for (auto it = begin; it != end; ++it) {
+		result.push_back(it->str());
+	}
+	std::cout << input_str;
+	return result;
 }
 
 void test1() {
-    catnet::Logger::ptr logger(new catnet::Logger("ROOT"));
-    logger->setLevel(catnet::LogLevel::DEBUG);
-    CATNET_LOG_INFO(logger) << "Hello World!";
-    CATNET_LOG_DEBUG(logger) << "1111111111";
-    CATNET_LOG_ERROR(logger) << "2222222222";
-    CATNET_LOG_ERROR(logger) << "3333333333";
-    CATNET_LOG_ERROR(logger) << "44444444444";
-    catnet::LogFormatter::ptr formatter(new catnet::LogFormatter("%d [%p] %f-%l %m %[ %%"));
-    formatter->pattern();
+	catnet::Logger::ptr logger(new catnet::Logger("ROOT"));
+	logger->setLevel(catnet::LogLevel::DEBUG);
+	CATNET_LOG_INFO(logger) << "Hello World!";
+	CATNET_LOG_DEBUG(logger) << "1111111111";
+	CATNET_LOG_ERROR(logger) << "2222222222";
+	CATNET_LOG_ERROR(logger) << "3333333333";
+	CATNET_LOG_ERROR(logger) << "44444444444";
+	const char *format_temp = "%d [%p] %f-%l %m %[ %%";
+	catnet::LogFormatter::ptr formatter(new catnet::LogFormatter(format_temp));
+	formatter->pattern();
+	// std::cout<<formatter->getItems();
+	std::cout << "func test1() formatter:" << format_temp << std::endl;
+	for (const auto& item: formatter->getItems()) {
+		std::cout << item << std::endl;
+	}
 }
 
 void test2() {
-    std::string input_str = "%d [%p] %f-%l %m %[ %%";
-    
-    std::vector<std::string> result = split_string(input_str);
-    
-    // 打印拆分结果
-    for (const auto& item: result) {
-        std::cout << "'" << item << "'" << std::endl;
-    }
+	std::string input_str = "%d [%p] %f-%l %m %[ %%";
+
+	std::vector<std::string> result = split_string(input_str);
+
+	// 打印拆分结果
+	for (const auto& item: result) {
+		std::cout << "'" << item << "'" << std::endl;
+	}
 }
 
 int main() {
-    test1();
-//    test2();
-    return 0;
+	test1();
+	//    test2();
+	return 0;
 }
